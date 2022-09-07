ede7b3f6bbf0de24b281096cd9ea71dd33a57e52
akapp9709 <2329108@students.wits.ac.za>
2022-09-07 09:46:56 +0200

    Movement in (basic)

--- GD3B-Exam/Assets/Scripts/Character Control/Look.cs
+++ GD3B-Exam/Assets/Scripts/Character Control/Look.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Look : MonoBehaviour
 {
+    [SerializeField] private Transform camAnchor;
     // Start is called before the first frame update
     void Start()
     {
--- GD3B-Exam/Assets/Scripts/Character Control/Movement.cs
+++ GD3B-Exam/Assets/Scripts/Character Control/Movement.cs
@@ -1,18 +1,37 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Movement : MonoBehaviour
 {
+    [SerializeField] private Vector3 moveVec;
+
+    [SerializeField] private float moveSpeed;
+    private Rigidbody _rb;
+    private Vector2 _inVec;
     // Start is called before the first frame update
     void Start()
     {
-        
+        _rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void FixedUpdate()
+    {
+        _rb.velocity = moveVec * moveSpeed;
+    }
+
+    private void OnMove(InputValue value)
+    {
+        var temp = value.Get<Vector2>();
+        _inVec = temp.normalized;
+        moveVec = new Vector3(_inVec.x, 0f, _inVec.y);
     }
 }
