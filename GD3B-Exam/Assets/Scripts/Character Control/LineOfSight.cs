92f4093d06e830600b8d34f124442d29a37ec840
akapp9709 <2329108@students.wits.ac.za>
2022-09-14 19:10:57 +0200

    Updated line of sight to be more accessible

--- GD3B-Exam/Assets/Scripts/Character Control/LineOfSight.cs
+++ GD3B-Exam/Assets/Scripts/Character Control/LineOfSight.cs
@@ -5,16 +5,18 @@ using UnityEngine;
 public class LineOfSight : MonoBehaviour
 {
     public Transform lookPnt;
+
+    public RaycastHit hit;
     // Start is called before the first frame update
     void Start()
     {
-        
+        hit = new RaycastHit();
     }
 
     // Update is called once per frame
     void Update()
     {
-        var hit = new RaycastHit();
+        
         var temp = new Vector3();
 
         if (Physics.Raycast(transform.position, transform.forward, out hit))
