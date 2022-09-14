ca1a41c1b940f6681a41207b92864f09762bff7b
akapp9709 <2329108@students.wits.ac.za>
2022-09-14 19:01:29 +0200

    Backbone script for interaction

--- GD3B-Exam/Assets/Scripts/Character Control/LineOfSight.cs
+++ GD3B-Exam/Assets/Scripts/Character Control/LineOfSight.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ItemHighlight : MonoBehaviour
+public class LineOfSight : MonoBehaviour
 {
     public Transform lookPnt;
     // Start is called before the first frame update
