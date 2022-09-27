74d6c029bdf7f5121b878825b7485852f7734b7e
akapp9709 <2329108@students.wits.ac.za>
2022-09-27 13:25:59 +0200

    Block Working
    Needs testing with actual enemies

--- GD3B-Exam/Assets/Scripts/Character Control/OffHandControl.cs
+++ GD3B-Exam/Assets/Scripts/Character Control/OffHandControl.cs
@@ -5,15 +5,25 @@ using UnityEngine;
 public class OffHandControl : MonoBehaviour
 {
     [SerializeField] private GameObject offHand;
+
+    private Animator _anim;
     // Start is called before the first frame update
     void Start()
     {
-        
+        _anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void OnOffHandActivate()
+    {
+        var temp = offHand.GetComponentInChildren<Offhand>().ActivateAbility();
+        Debug.Log(temp);
+        var bTemp = _anim.GetBool(temp);
+        _anim.SetBool(temp, !bTemp);
     }
 }
---
+++ GD3B-Exam/Assets/Scripts/Character Control/Shield.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shield : Offhand
+{
+    public float mitigation;
+    public string animationParamName;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public override string ActivateAbility()
+    {
+        return animationParamName;
+    }
+}
