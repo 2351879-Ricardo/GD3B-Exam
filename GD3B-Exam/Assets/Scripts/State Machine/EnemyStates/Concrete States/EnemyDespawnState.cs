20d59da9571d89833c4bffba29f31bf8ca8a0025
2351879-Ricardo <2351879@students.wits.ac.za>
2022-10-17 14:17:48 +0200

    Update EnemyDespawnState.cs

--- GD3B-Exam/Assets/Scripts/State Machine/EnemyStates/Concrete States/EnemyDespawnState.cs
+++ GD3B-Exam/Assets/Scripts/State Machine/EnemyStates/Concrete States/EnemyDespawnState.cs
@@ -4,10 +4,15 @@ public class EnemyDespawnState : EnemyBaseState
 {
     public override void EnterState(EnemyStateManager enemy)
     {
-        GameObject.Destroy(enemy.gameObject);
+        Object.Destroy(enemy.gameObject);
     }
 
     public override void UpdateState(EnemyStateManager enemy)
+    {
+        
+    }
+
+    public override void EndAction(int i)
     {
         
     }
