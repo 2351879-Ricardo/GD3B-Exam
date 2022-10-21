3947e7472dd9019d64d11ab3dbfb98c507fa35ed
AshleyCJ <61482642+AshleyCJ@users.noreply.github.com>
2022-10-21 21:20:59 +0200

    Added arena doors

---
+++ GD3B-Exam/Assets/Scripts/MapGeneration/ArenaDoors.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArenaDoors : MonoBehaviour
+{
+    public GameObject Player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Player = GameObject.Find("TestPlayer");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider collision)
+    {
+        if (collision.gameObject == Player)
+        {
+            //Debug.Log("Entered door");
+            //Call script to move player to arena
+        }
+    }
+}
