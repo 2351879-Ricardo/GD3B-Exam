17dfd2e342e7cd391aab85a85a7a7d4ce220f663
akapp9709 <2329108@students.wits.ac.za>
2022-11-07 00:38:31 +0200

    Boss Charge Fnctionality
    Still a bit weird. Charges toward centre before charging towards the player.

--- GD3B-Exam/Assets/Scripts/State Machine/EnemyStates/Concrete States/EnemyChargeState.cs
+++ GD3B-Exam/Assets/Scripts/State Machine/EnemyStates/Concrete States/EnemyChargeState.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.AI;
 
@@ -7,10 +8,17 @@ public class EnemyChargeState : EnemyBaseState
 {
     private float _chargeTime;
     private float _normalSpeed, _chargeSpeed;
+    private EnemyStateManager _enemy;
+    private Vector3 _targetDir;
+
+    public void OnDrawGizmos()
+    {
+        Gizmos.DrawRay(_enemy.transform.position, _targetDir);
+    }
     
     public override void EnterState(EnemyStateManager enemy)
     {
-        Debug.Log("Entered Charge State");
+        _enemy = enemy;
 
         _normalSpeed = enemy.EnemyController.EnemySo.EnemySpeed;
         _chargeSpeed = _normalSpeed * enemy.EnemyController.EnemySo.ChargeMult;
@@ -18,6 +26,8 @@ public class EnemyChargeState : EnemyBaseState
         enemy.EnemyController.gameObject.GetComponent<NavMeshAgent>().speed = 0;
         enemy.enemyAnimator.SetBool("isCharging", true);
         _chargeTime = 0;
+        _targetDir =  - _enemy.PlayerGameObject.transform.position - _enemy.transform.position;
+        _targetDir = _targetDir.normalized;
     }
 
     public override void UpdateState(EnemyStateManager enemy)
@@ -26,7 +36,6 @@ public class EnemyChargeState : EnemyBaseState
 
         if (_chargeTime >= enemy.actionTime)
         {
-            Debug.Log($"switching   {_chargeTime}");
             enemy.enemyAnimator.SetBool("isCharging", false);
             _chargeTime = 0;
             enemy.SwitchState(enemy.ChaseState);
@@ -35,6 +44,8 @@ public class EnemyChargeState : EnemyBaseState
 
     public override void EndAction(int i)
     {
+        _enemy.EnemyController.gameObject.GetComponent<NavMeshAgent>().speed = _chargeSpeed;
         
+        _enemy.gameObject.GetComponent<NavMeshAgent>().velocity = _targetDir * _chargeSpeed;
     }
 }
